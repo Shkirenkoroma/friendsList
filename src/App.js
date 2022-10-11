a435a89dd333014f27ebef2c8a15c3e44128b6bc
romajusik <shkirenkoroma@mail.ru>
2022-10-11 19:19:47 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.scss';
+import { Success } from './components/Success';
+import { Users } from './components/Users';
+import { useEffect } from 'react';
+
+
 
 function App() {
+ const [users, setUsers] = useState([]);
+ const [isLoading, setLoading] = useState(true);
+ const [searchValue, setSearchValue] = useState('');
+ const [invites, setInvites] = useState([]);
+ const [success, setSuccess] =useState(false)
+ useEffect(()=>{
+  fetch('https://reqres.in/api/users')
+ .then((response) => response.json())
+ .then((json)=>{setUsers(json.data);
+})
+ .catch((err)=>{console.warn(err);
+  alert('Ошибка при получении пользователей');}).finally(()=>setLoading(false));}
+, []);
+
+const onChangeSearchValue = (event) => {
+  setSearchValue(event.target.value);
+};
+const onCLickInvite = (id) => {
+  if (invites.includes(id)){
+    setInvites((prev) => prev.filter((_id)=> _id !== id));
+  } else {
+    setInvites((prev)=> [...prev, id]);
+  }
+};
+
+const onClickSendInvites = () =>{
+  setSuccess(true);
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {success ? (<Success count={invites.length}/> ) : ( <Users 
+      onChangeSearchValue={onChangeSearchValue} 
+      searchValue={searchValue} 
+      items={users} 
+      isLoading={isLoading} 
+      invites={invites}
+      onCLickInvite={onCLickInvite}    
+      onClickSendInvites={onClickSendInvites}     
+      />)}
+       
     </div>
   );
 }
